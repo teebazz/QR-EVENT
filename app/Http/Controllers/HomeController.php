a8df96a0059093a8de62d696262b5c943d0a7f50
badejo oluwatobi <teebazzy@gmail.com>
2021-05-07 20:01:11 +0100

    verify page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -24,5 +24,11 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+
+    public function verify()
+    {
+        return view('verify-qr');
     }
 }
